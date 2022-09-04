6f170d9accf286dac8b731c5f01cb9cd4c201de6
Elias <etav@berkeley.edu>
2022-09-03 19:35:15 -0500

    Add vis features to dashboard

--- app/main.py
+++ app/main.py
@@ -1,11 +1,33 @@
+from distutils.command.clean import clean
 import streamlit as st
 import pandas as pd
 import numpy as np
-from util.nba_data import get_season_data
+from util.nba_data import get_season_data, clean_data
+import plotly.figure_factory as ff
 
 def streamlit_app():
+    
+    # Titleheader
     st.title('NBA Stats')
 
+    # Create list to get the different season from 2013-2022
+    nba_seasons = ['20' + str(i) + '-' + str(i+1) for i in range(13, 22)][::-1]
+    selection = st.selectbox('NBA Season:', nba_seasons)
+
+    # Output some data
+    df = get_season_data(selection)
+    df = clean_data(df)
+    st.table(df.head(10))
+
+    # Setting metrics:
+    col1, col2, col3 = st.columns(3)
+    col1.metric(label='Max Points Scored:', value=df['PTS'].max())
+    col2.metric(label='Player', value=df[df['PTS'] == df['PTS'].max()]['PLAYER_NAME'].iloc[0])
+
+
+    # Plot a simple graph
+    fig = ff.create_distplot([df['PTS']], group_labels=['PTS'], bin_size=100)
+    st.plotly_chart(fig, use_container_width=True)
 
 if __name__ == '__main__':
     streamlit_app()
--- app/util/nba_data.py
+++ app/util/nba_data.py
@@ -1,13 +1,18 @@
 from nba_api.stats.endpoints import leaguedashplayerstats
+import pandas as pd
 
 
 def get_season_data(season: str):
     """Retrieves the season data for the desired season. I believe this only goes back to 2013. 
 
     Args:
-        season (str): NBA season in the following format: YYYY-YY
+        szn (str): NBA season in the following format: YYYY-YY
 
     Returns:
         pd.DataFrame: Returns a dataframe containing information for each player. 
     """
-    return leaguedashplayerstats.LeagueDashPlayerStats(season=season).get_available_data()[0]
+    return leaguedashplayerstats.LeagueDashPlayerStats(season=season).get_data_frames()[0]
+
+
+def clean_data(df: pd.DataFrame):
+    return df.drop(['PLAYER_ID', 'NICKNAME', 'TEAM_ID', 'WNBA_FANTASY_PTS_RANK', 'CFID', 'CFPARAMS'], axis=1)
